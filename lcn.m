c1fd5d75154d7b0fccf5272bbef2f8ad098e538b
boris <boris@inb.uni-luebeck.de>
2016-06-28 21:22:39 +0300

    tried other Gaussian windows

--- lcn.m
+++ lcn.m
@@ -16,16 +16,16 @@ if (length(sz) < 4), sz(4) = 1; end
 sigma = ceil(sz(1)/16); % Gaussian window sigma
 
 % initialize a Gaussian weighting window
-w_pq = repmat(single(fspecial('gaus', sigma*4+1, sigma*1.5)), [1,1,sz(3)]); % sum(w(:)) = sz(3)
+w_pq = repmat(single(fspecial('gaus', round(sigma*4+1), sigma*1.5)), [1,1,sz(3)]); % sum(w(:)) = sz(3)
 w_pq = w_pq./sz(3); % so that sum for all windows = 1, sum_ipq(w) = 1
 if (gpu), w_pq = gpuArray(w_pq); end
 
 % Subtractive normalization
 if (is_vl)
-    x_ijk = bsxfun(@minus, x_ijk, vl_nnconv(x_ijk, w_pq, [], 'pad', sigma*2)); % v_ijk 
+    x_ijk = bsxfun(@minus, x_ijk, vl_nnconv(x_ijk, w_pq, [], 'pad', round(sigma*2))); % v_ijk 
 else
     w_pq = padarray(w_pq, sz(1:2)-1,0,'post');
-    x_ijk = padarray(x_ijk, repmat(sigma*4+1,1,2)-1,0,'post');
+    x_ijk = padarray(x_ijk, repmat(round(sigma*4+1),1,2)-1,0,'post');
     for d=1:2, w_pq = fft(w_pq,[],d);  end
     x_ijk = bsxfun(@minus, x_ijk, conv_fft(fft2(x_ijk), w_pq, sz, false)); % v_ijk 
 end
@@ -33,7 +33,7 @@ end
 % Divisive normalization
 % weighted standard deviation of all features over a spatial neighborhood
 if (is_vl)
-    sigma_jk = sqrt(vl_nnconv(x_ijk.^2, w_pq, [], 'pad', sigma*2));
+    sigma_jk = sqrt(vl_nnconv(x_ijk.^2, w_pq, [], 'pad', round(sigma*2)));
 else
     sigma_jk = sqrt(conv_fft(fft2(x_ijk.^2), w_pq, sz, true));
     offset = floor(([size(x_ijk,1),size(x_ijk,2)] - sz(1:2))./2);
