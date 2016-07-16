0f0dc741342df2b33da2eecb0d894a2ecffbdc8f
boris <boris@inb.uni-luebeck.de>
2016-07-16 10:09:49 +0300

    visualization of rectangular images fixed

--- imsetshow.m
+++ imsetshow.m
@@ -25,7 +25,7 @@ end
 im = zeros(m*sz(2),n*sz(1),sz(3));
 for i=1:sz(4)
     [k1,k2] = ind2sub([m,n],i);
-    im(sz(2)*(k1-1)+1:k1*sz(2),sz(1)*(k2-1)+1:k2*sz(1),:) = mat2gray(images(:,:,:,i));
+    im(sz(1)*(k1-1)+1:k1*sz(1),sz(2)*(k2-1)+1:k2*sz(2),:) = mat2gray(images(:,:,:,i));
 end
 im = mat2gray(im);
 imshow(im);
